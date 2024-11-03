38294b3918098a669c93b847c963ed2933196421
pavanborra235 <125708327+pavanborra235@users.noreply.github.com>
2024-11-03 09:52:18 -0500

    Add files via upload

---
+++ src/main/java/com/example/openshift/demo/SpringDemo1Application.java
@@ -0,0 +1,26 @@
+package com.example.openshift.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo1Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to Deloitte";
+	}
+	
+	@GetMapping("/{input}")
+	public String welcomeUser(@PathVariable String input) {
+		return "Welcome to Deloitte " +input+" Deployment successful";
+	}
+}
